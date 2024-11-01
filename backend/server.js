5a7fe36fd6dda044c4acecee992d0a4886dd030d
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-11-01 16:09:49 +0100

    Add routing

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 require("dotenv").config()
 
 const express = require('express');
+const workoutRoutes = require("./routes/workouts")
 
 const app = express();
+
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
-app.get('/', (req, res) => {
-  res.json({mssg: "Welcome to the app"})
-})
+app.use("/api/workouts", workoutRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port', process.env.PORT)
