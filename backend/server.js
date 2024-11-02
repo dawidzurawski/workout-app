cf7cdbd69c8955129f6f9624f10dffe5d81f03d3
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-11-02 10:06:56 +0100

    Connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require("./routes/workouts")
 
 const app = express();
@@ -14,8 +15,14 @@ app.use((req, res, next) => {
 
 app.use("/api/workouts", workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
 process.env
